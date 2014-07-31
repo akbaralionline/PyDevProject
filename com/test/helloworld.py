acf0003df28fa745d5b26439c6d67e75ff3f8716
akbaralionline <akbaralionline@gmail.com>
2014-07-31 16:08:15 -0400

    sample code

--- com/test/helloworld.py
+++ com/test/helloworld.py
@@ -1,10 +1,18 @@
-'''
-Created on Jul 29, 2014
+def find_start_point(p, d):
+    # Check for if solution exist
+    if sum(p) < sum(d):
+        print ('Solution do not exist')
+        return None
+    startpoint, ssum = 0, 0
+    for i in range(0, len(p)):
+        ssum+=p[i]-d[i]
+        if ssum < 0:
+            # Petrol pumps will lie in the forward indexes
+            startpoint = i+1
+            ssum = 0
+    return p[startpoint]
 
-@author: mohammad.ali
-'''
-
-print("Most welcome")
-print("Minor change in the file")
-print("few Minor change in the file")
-print("few Major change in the file")
+if __name__=='__main__':
+    p = [6,7,1,4]
+    d = [5,8,3,2]
+    print(find_start_point(p, d))
